9c13c5bd516d270af83b4449650b90ef9c6988c9
Zachary Stence <zachstence@gmail.com>
2023-09-02 16:08:14 -0500

    feat: better progress logging

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,17 @@ import { format } from 'date-fns'
 export const main = async (): Promise<void> => {
     try {
         const { inputCsvFilePath, asOfDate } = cliArgsSchema.parse(process.argv)
-        console.log(inputCsvFilePath, asOfDate)
+
+        console.log(`Reading ${inputCsvFilePath}...`)
 
         const bondsCsv = await readFile(inputCsvFilePath)
         const bonds = bondsCsvSchema.parse(parseCsv(bondsCsv))
 
+        console.log(`Calculating value of ${bonds.length} bonds${asOfDate ? `as of ${asOfDate?.toLocaleTimeString()}` : ''}...`)
+
         const { valuesAsOfDate, bondValues } = await calculateBondValues(bonds, asOfDate)
         console.table(bondValues)
-    
+
         console.log('Values as of date', format(valuesAsOfDate, 'MM/yyyy'))
     
         const totalValue = bondValues.reduce<number>((acc, bondValue) => acc += bondValue.value, 0)
