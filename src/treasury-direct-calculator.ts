e45ed802cc2e568b0c39aa376ecc5f3a306945c5
Zachary Stence <zachstence@gmail.com>
2023-09-02 13:13:31 -0500

    chore: remove unnecessary headers

--- src/treasury-direct-calculator.ts
+++ src/treasury-direct-calculator.ts
@@ -47,24 +47,8 @@ export const calculateBondValue = async (bond: Bond, redemptionDate = new Date()
     try {
         const res = await client.post('/BC/SBCPrice', data, {
             headers: {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'Accept-Language': 'en-US,en;q=0.9',
                 'Cache-Control': 'no-cache',
-                'Connection': 'keep-alive',
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'DNT': '1',
-                'Origin': 'https://treasurydirect.gov',
-                'Pragma': 'no-cache',
-                'Referer': 'https://treasurydirect.gov/BC/SBCPrice',
-                'Sec-Fetch-Dest': 'document',
-                'Sec-Fetch-Mode': 'navigate',
-                'Sec-Fetch-Site': 'same-origin',
-                'Sec-Fetch-User': '?1',
-                'Upgrade-Insecure-Requests': '1',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-                'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
             }
         })
     
